cf1aadacc2ed327ae5d539d5e29c2f535bf01df5
phoenixmnk <phoenixcanyonon@gmail.com>
2021-02-15 17:39:04 -0800

    array problems

---
+++ array/combination_sum.py
@@ -0,0 +1,57 @@
+# Given a set of candidate numbers (candidates) (without duplicates) and a target number (target), find all unique combinations in candidates where the candidate numbers sums to target.
+#
+# The same repeated number may be chosen from candidates unlimited number of times.
+#
+# Note:
+#
+# All numbers (including target) will be positive integers.
+# The solution set must not contain duplicate combinations.
+# Example 1:
+#
+# Input: candidates = [2,3,6,7], target = 7,
+# A solution set is:
+# [
+#   [7],
+#   [2,2,3]
+# ]
+# Example 2:
+#
+# Input: candidates = [2,3,5], target = 8,
+# A solution set is:
+# [
+#   [2,2,2,2],
+#   [2,3,3],
+#   [3,5]
+# ]
+
+class Solution():
+
+    def combination_sum(self,candidates,target):
+
+        Solution.res=[]
+
+
+        self.dfs(candidates,target,0,[])
+        #print("iteration")
+        return Solution.res
+
+
+    def dfs(self,candidates,target,start,Values):
+
+        l= len(candidates)
+        if target ==0:
+            return Solution.res.append(Values)
+
+        #print Values,start,l
+        for i in range(start,l):
+            print(i,candidates[i],target)
+            if candidates[i] > target:
+                return
+            print("iteration1", candidates, target-candidates[i],i,Values+[candidates[i]])
+            self.dfs(candidates,target-candidates[i],i,Values+[candidates[i]])
+
+candidates = [2,3,5]
+target = 8
+
+s=Solution()
+print(s.combination_sum(candidates,target))
---
+++ array/kth_missing_element.py
@@ -0,0 +1,78 @@
+# Given a sorted array A of unique numbers, find the K-th missing number starting from the leftmost number of the array.
+#
+#
+#
+# for num
+#
+# Example 1:
+# Input: A = [4,7,9,10], K = 1
+# Output: 5
+# Explanation:
+# The first missing number is 5.
+#
+# Example 2:
+# Input: A = [4,7,9,10], K = 3
+# Output: 8
+# Explanation:
+# The missing numbers are [5,6,8,...], hence the third missing number is 8.
+#
+# Example 3:
+#
+# Input: A = [1,3,5], K = 15
+# [2,4,6,7,8,9,10,11,12,13,14,15,16,17,18]
+#
+# Note:
+#
+# 1 <= A.length <= 50000
+# 1 <= A[i] <= 1e7
+# 1 <= K <= 1e8
+
+##https://medium.com/@edward.zhou/leet-code-1060-missing-element-in-sorted-array-explained-python3-solution-a1277b6ce32b
+
+
+
+class Solution():
+
+    def missing_element(self,nums,k):
+
+        missing_nums=0
+        for i in range(1,len(nums)):
+            print(nums[i],nums[i-1])
+            if nums[i]!=nums[i-1]+1:
+                missing_nums+=(nums[i]-nums[i-1])-1
+
+        def missing_search(left, right, newk):
+            print(left, right, newk)
+            if left == right:
+                return None
+
+            if left+1 == right:
+                return nums[left]+newk
+
+            middle = (left+right)/2
+
+            totalMissingNum = (nums[middle] - nums[left]) - (middle - left)
+
+            #print totalMissingNum
+
+            if newk > totalMissingNum:
+                #print("new",middle, right, newk - totalMissingNum)
+                return missing_search(middle, right, newk - totalMissingNum)
+            else:
+                return missing_search(left, middle, newk)
+
+
+
+        if nums[-1]-missing_nums > k:
+            return missing_search(0,len(nums)-1,k)
+        else:
+            return nums[missing_nums]+k-missing_nums
+
+
+A = [1,3,5]
+K = 1
+s=Solution()
+print(s.missing_element(A,K))
+
+
+
